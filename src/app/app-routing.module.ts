af96cbed86b2d990a68b7808abf41d51257d93fa
rbxrt <rdieterle@posteo.de>
2022-08-02 23:46:47 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
+import { SettingsComponent } from './settings/settings.component';
+import { StatsComponent } from './stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'play', component: PlayComponent },
+  { path: 'stats', component: StatsComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayComponent } from './play/play.component';
+import { StatsComponent } from './stats/stats.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayComponent,
+    StatsComponent,
+    SettingsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
